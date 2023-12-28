ba1d017e79708e66f9eb8eae18c68deeb94f2cd7
samuelevalperta <samuele.valperta@gmail.com>
2023-12-29 00:13:29 +0100

    Add DBUS_SESSION_BUS_ADDRESS env on battery script

--- scripts/battery-low.sh
+++ scripts/battery-low.sh
@@ -3,6 +3,9 @@
 DISCHARGING="$(cat /sys/class/power_supply/BAT0/status | grep 'Discharging')"
 BATTERY_LEVEL="$(cat /sys/class/power_supply/BAT0/capacity)"
 
-if [ ! -z "${DISCHARGING}" ] && [ "${BATTERY_LEVEL}" -le 10 ]; then
+DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+export DBUS_SESSION_BUS_ADDRESS
+
+if [ ! -z "${DISCHARGING}" ] && [ "${BATTERY_LEVEL}" -le 15 ]; then
   notify-send --icon=battery-low -u critical "Battery level is ${BATTERY_LEVEL}%!"
 fi
